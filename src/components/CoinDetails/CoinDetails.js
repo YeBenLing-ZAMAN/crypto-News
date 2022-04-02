1f41f7c4a41b62417ec4e9b8ea1d7d415606780a
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-02 23:22:01 +0600

    spinner added

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
 const CoinDetails = () => {
     const { id } = useParams();
     const [coin, setCoin] = useState({});
+
+    
+    /* spinner add */
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
         const url = `https://api.coingecko.com/api/v3/coins/${id}`
         fetch(url)
             .then(res => res.json())
             .then(data => setCoin(data));
+        setLoading(false);
     }, [id])  // id upre depend kore useEffect abar call hobe 
     return (
-        <div className='px-4 h-[90vh] pt-20 pb-24 mx-auto max-w-7xl md:p-2'>
+<>
+{
+    loading 
+    ?
+     (<Spinner></Spinner>)
+    :(
+    <div className='px-4 h-[90vh] pt-20 pb-24 mx-auto max-w-7xl md:p-2'>
             <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 content-center'>
                 <div className='order-2 md:order-1'>
                     <h1 className='text-3xl'>General Info:</h1>
@@ -28,8 +42,10 @@ const CoinDetails = () => {
                     <img src={coin.image?.large} alt="" />
                 </div>
             </div>
-
-        </div>
+        </div>)
+}
+</>
+        
     );
 };
 
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,29 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import CoinCard from '../CoinCard/CoinCard';
+import Spinner from '../Spinner/Spinner';
+import Contacts from '../Contacts/Contacts';
 
 const Coins = () => {
+
+    /* spinner add */
+    const [loading, setLoading] = useState(false);
+
+
+
     const [coins, setCoins] = useState([]);
     useEffect(() => {
+        setLoading(true);  // for spinner it's start 
         fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`)
             .then(res => res.json())
             .then(data => setCoins(data))
+
+        setLoading(false);  // for spinner it's turn off 
     }, [])
 
     return (
-        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2 bg-cyan-200'>
-            <p className='text-center text-3xl font-bold text-gray'>
-                Available Cypto Currencies
-            </p>
-            <p className='text-center mt-4 mb-12 text-xl font-normal text-gray-500'>
-                Total Coins :{coins.length}
-            </p>
+        <>
+            {loading
+                ?
+              (<Spinner></Spinner>)
+                :
+                (<div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2 bg-cyan-200'>
+                    <p className='text-center text-3xl font-bold text-gray'>
+                        Available Cypto Currencies
+                    </p>
+                    <p className='text-center mt-4 mb-12 text-xl font-normal text-gray-500'>
+                        Total Coins :{coins.length}
+                    </p>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 xl:grid-cols-4'>
-                {
-                    coins.map(coin => <CoinCard key={coin.key} coin={coin}></CoinCard>)
-                }
-            </div>
-        </div>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 xl:grid-cols-4'>
+                        {
+                            coins.map(coin => <CoinCard key={coin.key} coin={coin}></CoinCard>)
+                        }
+                    </div>
+                </div>)
+            }
+        </>
     );
 };
 
