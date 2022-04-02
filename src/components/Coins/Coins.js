d292f469670448c31a6d3d0223c2aab61e04ac51
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-04-02 19:09:51 +0600

    data sucessfully fetch from api

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Coins = () => {
+    const [coins, setCoins] =useState([]);
+    useEffect(()=>{
+        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`)
+        .then(res=>res.json())
+        .then(data=>setCoins(data))
+    },[])
+    
     return (
         <div>
-            this is coins pages 
+            {
+                <h1>coins :{coins.length}</h1>
+            }
         </div>
     );
 };
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
         <div className='md:px-16 py-8 bg-cyan-200'>
-            <div className='flex flex-col justify-center h-[80vh] items-center'>
+            <div className='flex flex-col justify-center h-[80vh] items-center explore-btn'>
                 <h1 className=' font-mono text-xl md:text-5xl '>
-                    Welcome to CRYPTO CAFE<span className='text-6xl'>☕</span>
+                    Welcome to CRYPTO NEWS<span className='text-6xl'>☕</span>
                 </h1>
-                <button className='py-2 px-3 text-white mt-3 rounded-full bg-cyan-600'>
+                <button className='py-2 px-5 text-white mt-3 rounded-full bg-cyan-600 '>
                     Explore Coins
                 </button>
             </div>
